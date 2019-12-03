9c80ae66a32ecb60fc7e5d2064cfa9b5f6b71afc
Brigid <breedd218@gmail.com>
2019-12-03 08:25:56 -0500

    fixed >:(

--- javascript/simple_battleship.js
+++ javascript/simple_battleship.js
@@ -11,25 +11,24 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
- guess = prompt("Ready, aim, fire! Enter a number from 0 to 6:");
- if(guess < 0 || guess > 6){
-   alert("Please enter a valid number");
- }
- else{
-   guesses = guesses + 1;
+  guess = prompt("Ready, aim, fire! Enter a number from 0 to 6:");
  
-   if(guess == location1 || guess == location2 || guess == location3){
-     hits = hits + 1;
-     alert("Hit!");
-     if (hits == 3 {
-       isSunk = true;
-       alert("You sank my battleship!")
-     }
-   }
-   else{
-     alert("Miss");
-   }
- }
+  if(guess < 0 || guess > 6){
+    alert("Please enter a valid number");
+  } else {
+    guesses = guesses + 1;
+ 
+    if(guess == location1 || guess == location2 || guess == location3){
+      hits = hits + 1;
+      alert("Hit!");
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sank my battleship!")
+       }
+    } else {
+      alert("Miss");
+    }
+  }
 }
 var stats = "You took " + guesses + " guesses to sink the battleship, " + 
             "which means your accuracy was " + (3/guesses)*100 + "%";
